88b15900be9ce8835332d0a6cb27302bf221d11a
Jithin <cyb3rjithu@gmail.com>
2018-12-27 21:36:00 +0530

    update_player_moves function updated

--- game/tic_tac_toe.py
+++ game/tic_tac_toe.py
@@ -1,20 +1,32 @@
+import random
+
+from exceptions import InvalidMoveException
+
 
 class TicTacToe(object):
 
     def __init__(self, player_one_name, player_two_name, bot=False):
-        pass
-    
+        self.player_one_name = player_one_name
+        self.player_two_name = player_two_name
+        self.bot = bot
+        self.player_one_moves = set()
+        self.player_two_moves = set()
+        self.toss()
+
+    def toss(self):
+        if random.randint(1, 2) == 1:
+            self.player_one_turn = True
+        else:
+            self.player_one_turn = False
 
     def generate_moves(self, dimension=3):
-        '''Generate all possible combinations of the game board along with 
-            the winning positions. 
+        '''Generate all possible combinations of the game board along with
+            the winning positions.
         '''
         # to store winning combinations
         self.winning_combinations = []
         # to store all the moves available in a game
         self.possible_moves = []
-        # stores intermediate moves while generating the moves
-        inter_moves = set()
         # stores each row at a time, for winning moves
         row_set = set()
         # column storage, for winning moves
@@ -29,7 +41,7 @@ class TicTacToe(object):
                     diagonal_set1.add((ival, jval))
                     diagonal_set2.add((dimension - ival - 1, jval))
                 # for all possible combos
-                inter_moves.add((ival, jval))
+                self.possible_moves.append((ival, jval))
                 # winning row set
                 row_set.add((ival, jval))
                 # winning column set, interchange i and j here to get column
@@ -40,11 +52,35 @@ class TicTacToe(object):
             # reset the values here
             row_set = set()
             column_set = set()
-            self.possible_moves.append(inter_moves)
-            inter_moves = set()
         self.winning_combinations.append(diagonal_set1)
         self.winning_combinations.append(diagonal_set2)
         del diagonal_set1
         del diagonal_set2
         del column_set
         del row_set
+
+    def update_player_moves(self, player, choice):
+        '''Based on the player name update the move in their respective sets
+
+            Args:
+
+                player: str, name of the player
+                choice: tuple, contians the row, column valuse as tuple
+
+        '''
+        # first player
+        if self.player_one_turn:
+            self.player_one_moves.add(choice)
+            self.possible_moves.remove(choice)
+            self.player_one_turn = False
+        else:
+            self.player_two_moves.add(choice)
+            self.possible_moves.remove(choice)
+            self.player_one_turn = True
+
+    def mark_choice(self, player, choice):
+        '''Add the movement to respective player's choice set'''
+        if choice in self.possible_moves:
+            pass
+        else:
+            raise  InvalidMoveException("Not a valid choice of input")
